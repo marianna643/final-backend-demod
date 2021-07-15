99f2dfa17cb2757eae4e1b0aa42e87ab3de39578
marianna643 <trma0520@gmail.com>
2021-07-15 19:20:22 +0200

    add Demo Repository

---
+++ src/main/java/nl/novi/mariannat/repository/DemoRepository.java
@@ -0,0 +1,9 @@
+package nl.novi.mariannat.repository;
+
+import nl.novi.mariannat.domain.Demo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DemoRepository  {
+}
